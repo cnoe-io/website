076be5105b669849f63b4209c468d16307ac5d90
Nima Kaviani <nkaviani@amazon.com>
2023-02-07 11:20:16 -0800

    fix the dark theme

--- src/components/MissionVision/index.js
+++ src/components/MissionVision/index.js
@@ -29,7 +29,7 @@ function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--6')}>
       <div className="text--center">
-        <Svg color="#00568c" className={styles.featureSvg} role="img" />
+        <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
