5568d309142356b5f747775472e551cae572ef80
Ben Fields <techpotato22@gmail.com>
2023-03-28 18:29:15 -0700

    Added pop up functionality.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import MissionVision from "@site/src/components/MissionVision";
 
 import styles from "./index.module.css";
+const { useState } = React;
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -27,65 +28,190 @@ function HomepageHeader() {
     </header>
   );
 }
-class InteractiveDiagram extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className={styles.interactivediagramcontainer}>
-        <div className={styles.diagramcontent}>
-          <div className={styles.toprow}>
-            <div className={styles.toprowsensor}></div>
-            <div className={styles.toprow1}>
-              Top Deployment Targets <br /> (Cloud/ On-prem / Edge)
-            </div>
-            <div className={styles.toprow2}>Bottom This is text behind</div>
+const InteractiveDiagram = () => {
+  // and here we don't need to call React.useState because it has been
+  // separated into its own variable
+  const [promptText, setPromptText] = useState("");
+  const [isShow, setIsShow] = useState("none");
+  return (
+    <div className={styles.interactivediagramcontainer}>
+      <div className={styles.diagramcontent}>
+        <div className={styles.toprow}>
+          <div className={styles.toprowsensor}></div>
+          <div className={styles.toprow1}>
+            Top Deployment Targets <br /> (Cloud/ On-prem / Edge)
           </div>
-          <div className={styles.inner}></div>
-          <div className={styles.bottomrow}>
-            <div className={styles.bottomleftcol}>
-              <div className={styles.bottomleftcolleft}>Signing</div>
-              <div className={styles.bottomleftcolright}>
-                <div className={styles.cell}>Packaging/Templating</div>
-                <div className={styles.cell}>Code Repository</div>
-                <div className={styles.cell}>Config Repository</div>
-                <div className={styles.cell}>Artifact Registries</div>
-                <div className={styles.cell}>Secret Repository</div>
+          <div className={styles.toprow2}>Bottom This is text behind</div>
+        </div>
+        <div className={styles.inner}></div>
+        <div className={styles.bottomrow}>
+          <div className={styles.bottomleftcol}>
+            <div
+              className={styles.bottomleftcolleft}
+              onClick={() => {
+                setIsShow("block");
+                setPromptText("Signing");
+              }}
+            >
+              Signing
+            </div>
+            <div className={styles.bottomleftcolright}>
+              <div
+                className={styles.cell}
+                onClick={() => {
+                  setIsShow("block");
+                  setPromptText("Packaging/Templating");
+                }}
+              >
+                Packaging/Templating
+              </div>
+              <div
+                className={styles.cell}
+                onClick={() => {
+                  setIsShow("block");
+                  setPromptText("Code Repository");
+                }}
+              >
+                Code Repository
+              </div>
+              <div
+                className={styles.cell}
+                onClick={() => {
+                  setIsShow("block");
+                  setPromptText("Config Repository");
+                }}
+              >
+                Config Repository
+              </div>
+              <div
+                className={styles.cell}
+                onClick={() => {
+                  setIsShow("block");
+                  setPromptText("Artifact Registries");
+                }}
+              >
+                Artifact Registries
+              </div>
+              <div
+                className={styles.cell}
+                onClick={() => {
+                  setIsShow("block");
+                  setPromptText("Secret Repository");
+                }}
+              >
+                Secret Repository
               </div>
             </div>
-            <div className={styles.bottomrightcol}>
-              <div className={styles.bottomrightcolleft}>
-                <div className={styles.cell}>Developer Portal</div>
-                <div className={styles.cell}>Identity and Access</div>
-                <div className={styles.rowcell}>
-                  <div className={styles.cell}>
-                    Infra as <br /> Code
-                  </div>
-                  <div className={styles.cell}>
-                    Continuous <br /> Delivery
-                  </div>
-                  <div className={styles.cell}>
-                    Workflow <br /> Orchestration
-                  </div>
-                  <div className={styles.endcell}>
-                    Service <br /> Discovery
-                  </div>
-                </div>
-                <div className={styles.cell}>Secret Management</div>
-                <div className={styles.cell}>Validation</div>
-                <div className={styles.cell}>Compute Platform</div>
+          </div>
+          <div className={styles.bottomrightcol}>
+            <div className={styles.bottomrightcolleft}>
+              <div
+                className={styles.cell}
+                onClick={() => {
+                  setIsShow("block");
+                  setPromptText("Developer Portal");
+                }}
+              >
+                Developer Portal
               </div>
-              <div className={styles.bottomrightcolright}>Observability</div>
+              <div
+                className={styles.cell}
+                onClick={() => {
+                  setIsShow("block");
+                  setPromptText("Identity and Access");
+                }}
+              >
+                Identity and Access
+              </div>
+              <div className={styles.rowcell}>
+                <div
+                  className={styles.cell}
+                  onClick={() => {
+                    setIsShow("block");
+                    setPromptText("Infra as Code");
+                  }}
+                >
+                  Infra as <br /> Code
+                </div>
+                <div
+                  className={styles.cell}
+                  onClick={() => {
+                    setIsShow("block");
+                    setPromptText("Continuous Delivery");
+                  }}
+                >
+                  Continuous <br /> Delivery
+                </div>
+                <div
+                  className={styles.cell}
+                  onClick={() => {
+                    setIsShow("block");
+                    setPromptText("Workflow Orchestration");
+                  }}
+                >
+                  Workflow <br /> Orchestration
+                </div>
+                <div
+                  className={styles.endcell}
+                  onClick={() => {
+                    setIsShow("block");
+                    setPromptText("Service Discovery");
+                  }}
+                >
+                  Service <br /> Discovery
+                </div>
+              </div>
+              <div
+                className={styles.cell}
+                onClick={() => {
+                  setIsShow("block");
+                  setPromptText("Secret Management");
+                }}
+              >
+                Secret Management
+              </div>
+              <div
+                className={styles.cell}
+                onClick={() => {
+                  setIsShow("block");
+                  setPromptText("Validation");
+                }}
+              >
+                Validation
+              </div>
+              <div
+                className={styles.cell}
+                onClick={() => {
+                  setIsShow("block");
+                  setPromptText("Compute Platform");
+                }}
+              >
+                Compute Platform
+              </div>
+            </div>
+            <div
+              className={styles.bottomrightcolright}
+              onClick={() => {
+                setIsShow("block");
+                setPromptText("Observability");
+              }}
+            >
+              Observability
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+      <div className={styles.popUpPrompt} style={{ display: `${isShow}` }}>
+        <div className={styles.innerPromptBox}>
+          <div className={styles.closeIcon} onClick={() => setIsShow("none")}>
+            X
+          </div>
+          <div className={styles.promptBody}>{promptText}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
